da8ab2455229f5cbc970993a37b8454f58200472
zetsuna <felsa.jr1205@gmail.com>
2025-12-24 08:01:00 +0700

    delete some file dump

--- frontend/src/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import HeaderSearch from "./HeaderSearch";
-
-export default function Header() {
-  return (
-    <div className="border-b border-[#F0F3F7] h-[80px] fixed top-0 w-full z-[99999] bg-white">
-      <HeaderSearch />
-    </div>
-  );
-}
